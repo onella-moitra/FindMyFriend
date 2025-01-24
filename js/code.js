5857c2f5602115c0f5212c3acf53d7ff1a5550d7
skeeper12 <adrian82009@hotmail.com>
2025-01-24 13:39:06 -0500

    test

--- js/code.js
+++ js/code.js
@@ -5,17 +5,18 @@ let userId = 0;
 let firstName = "";
 let lastName = "";
 
-
+/*
 const myButton = document.getElementById('loginButton');
 
 myButton.addEventListener('click', () => {
-	console.log("CLick")
+	console.log("CLick");
    doLogin(); 
 });
+*/
 
 function doLogin()
 {
-	console.log("Logging in")
+	console.log("Logging in");
 	userId = 0;
 	firstName = "";
 	lastName = "";
