339a32fbf0a8eb29e4ca5afe5b55904e37c1b686
Caleb <belacile13@gmail.com>
2025-02-04 16:17:54 -0500

    Added UpdateContacts.php for testing

---
+++ LAMPAPI/UpdateContacts.php
@@ -0,0 +1,48 @@
+<?php
+
+	$inData = getRequestInfo();
+
+	$phoneNumber = $inData["phoneNumber"];
+	$emailAddress = $inData["emailAddress"];
+	$newFirst = $inData["newFirstName"];
+	$newLast = $inData["newLastName"];
+	$contactId = $inData["contactId"];
+
+
+	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
+		if ($conn->connect_error)
+		{
+			returnWithError( $conn->connect_error );
+		}
+		else
+		{
+			$stmt = $conn->prepare("UPDATE Contacts SET FirstName = ?, LastName=?, Phone= ?, Email= ? WHERE ID= ?");
+			$stmt->bind_param("ssssi", $newFirst, $newLast, $phoneNumber, $emailAddress, $contactId);
+			$stmt->execute();
+
+			$stmt->close();
+			$conn->close();
+			returnWithError("");
+		}
+
+
+
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+
+	function returnWithError( $err )
+	{
+		$retValue = '{"contactId":0,"firstName":"","lastName":"","error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+
+
+?>
